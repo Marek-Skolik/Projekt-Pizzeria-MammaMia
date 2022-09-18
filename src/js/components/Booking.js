fa5cde4f89d84d7db3e3268ceb8e46ab34f9269f
Marek Skolik <marek250296@interia.pl>
2022-09-18 20:39:43 +0200

    error404

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -10,17 +10,17 @@ class Booking {
     
     thisBooking.render(element);
     thisBooking.initWidgets();
-    thisBooking.getData();
+    thisBooking.getDate();
   }
 
-  getData(){
+  getDate(){
     const thisBooking = this;
 
-    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
-    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePickerElem.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePickerElem.maxDate);
 
     const params = {
-      booking: [
+      bookings : [
         startDateParam,
         endDateParam,
       ],
@@ -39,36 +39,36 @@ class Booking {
 
     const urls = {
       booking:       settings.db.url + '/' + settings.db.booking 
-                                     + '?' + params.booking.join('&'),
-      eventsCurrent: settings.db.url + '/' + settings.db.event
+                                     + '?' + params.bookings.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.event   
                                      + '?' + params.eventsCurrent.join('&'),
-      eventsRepeat:  settings.db.url + '/' + settings.db.event
+      eventsRepeat:  settings.db.url + '/' + settings.db.event   
                                      + '?' + params.eventsRepeat.join('&'),
     };
-    //console.log('getData urls', urls);
+
+    //console.log('getData urls', url);
 
     Promise.all([
       fetch(urls.booking),
       fetch(urls.eventsCurrent),
       fetch(urls.eventsRepeat),
     ])
-      .then(function(allResponses){
-        const bookingResponse = allResponses[0];
+      .then(function (allResponses) {
+        const bookingsResponse = allResponses[0];
         const eventsCurrentResponse = allResponses[1];
         const eventsRepeatResponse = allResponses[2];
         return Promise.all([
-          bookingResponse.json(),
+          bookingsResponse.json(),
           eventsCurrentResponse.json(),
           eventsRepeatResponse.json(),
         ]);
       })
-      .then(function([bookings, eventsCurrent, eventsRepeat]){
-        console.log(bookings);
-        console.log(eventsCurrent);
-        console.log(eventsRepeat);
+      .then(function ([bookings, eventsCurrent, eventsRepeat]) {
+        console.log('bookings', bookings);
+        console.log('eventsCurrent', eventsCurrent);
+        console.log('eventsRepeat', eventsRepeat);
       });
-    }
-   
+  }
 
   render(element){
     const thisBooking = this;
